711b479b820de0673cdf8d9929953349b76d08d2
jana <ramana.jana211@gmail.com>
2021-02-27 11:37:05 +0530

    3

---
+++ tw.sh
@@ -0,0 +1,3 @@
+five.sh     index1.html  login.groovy  pipeline.groovy	      test1
+in.py	    index2.html  one.sh        pipelinescript.groovy  tw.sh
+index.html  index3.html  pick.py       script.groovy
